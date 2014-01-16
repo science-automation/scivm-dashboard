fce6bff8ac256bb6f56cf97b856519f07c46c1c6
ubuntu <ubuntu@scivm.(none)>
2014-01-16 21:49:08 +0200

    add model for bucket

--- bucket/models.py
+++ bucket/models.py
@@ -8,17 +8,21 @@ from json_field import JSONField
 from scivm import utils
 
 class Bucket(models.Model):
-    uri = models.CharField(max_length=96, null=True, blank=True)
     name = models.CharField(max_length=96, null=True, blank=True)
     owner = models.ForeignKey(User, null=True, blank=True)
-    description = models.CharField(max_length=255, null=True, blank=True)
+    description = models.TextField(default="", blank=True)
     created = models.DateTimeField(auto_now_add=True)
-    properties = JSONField()
-    capacity = JSONField()
-    bootable = models.NullBooleanField(null=False, default=False)
-    supportsSnapshots = models.NullBooleanField(null=False, default=True)
-    snapshots = JSONField()
-    guestinterface = models.CharField(max_length=96, null=True, blank=True)
-    meters = JSONField()
-    eventlog = JSONField()
-    operations = JSONField()
+
+    # amazon ec2 access and secret key
+    access_key = models.CharField(max_length=96, null=True, blank=True)
+    access_secret_key = models.CharField(max_length=96, null=True, blank=True)
+
+
+class BucketFile(models.Model):
+    bucket_id = models.ForeignKey('Bucket')
+    owner = models.ForeignKey(User, unique=True)    
+    name = models.CharField(max_length=96, null=True, blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    lastmodified = models.DateTimeField(auto_now_add=True)
+    size = models.BigIntegerField(null=True, blank=True)
+    public = models.BooleanField(null=False, default=False)
