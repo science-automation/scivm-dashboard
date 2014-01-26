b9a3d3dce83989792edcc0376648568496925f15
Bence Tamas <mr.bence.tamas@gmail.com>
2014-01-26 11:09:05 +0100

    adapt ot changes in jobserver

--- jobs/management/commands/consume_job_results.py
+++ jobs/management/commands/consume_job_results.py
@@ -7,6 +7,7 @@ import redis
 import zlib
 import pickle
 from django.conf import settings
+import time
 
 class Command(BaseCommand):
     help = 'Consum custom job result queue'
@@ -14,12 +15,15 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         rcli = redis.StrictRedis(host=settings.REDIS_HOST, port=settings.REDIS_PORT, db=settings.REDIS_DB)
         while True:
-            queue, data = rcli.blpop("noq.jobs.finished")
+            queue, data = rcli.blpop("noq.jobs.updates")
             try:
                 update =  pickle.loads(zlib.decompress(data))
                 print update
-                job_data = {"pk": update.pop("pk"), "group_id": update.pop("group_id") }
-                print job_data, update
-                Job.backend.update_job_result.apply_async((job_data, update))
+                if update["type"] == "finished":
+                    update["finished_at"] = time.time()
+                    update.pop("type")
+                    job_data = {"pk": update.pop("pk"), "group_id": update.pop("group_id") }
+                    print job_data, update
+                    Job.backend.update_job_result.apply_async((job_data, update))
             except Exception, e:
                 print e
