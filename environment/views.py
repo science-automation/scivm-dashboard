dc79d5d19a5f78a8c7bbef44af0d4a7b519cf612
Bence Tamas <mr.bence.tamas@gmail.com>
2014-02-01 09:24:53 +0100

    move fav m2m fields from UserProfile to Image/Environment; clean up views

--- environment/views.py
+++ environment/views.py
@@ -15,33 +15,33 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Science VM. If not, see <http://www.gnu.org/licenses/>.
 
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods
 from django.core.urlresolvers import reverse
-from django.template import RequestContext
 from django.contrib import messages
 from django.utils.translation import ugettext as _
+from django.template.response import TemplateResponse
+from django import http
+
 from environment.models import Environment
 from image.models import Image
-from accounts.models import UserProfile
 from environment.forms import EnvironmentForm, EditEnvironmentForm
 from scivm import tasks
+
 
 @login_required
 def index(request):
     """Base view
     """
-    current_user = request.user
-    u = UserProfile.objects.get(id=current_user.id)
     ctx = {
-        'private_envs': Environment.objects.filter(owner__pk=current_user.pk),
-        'favorite_envs': u.favorite_env.all(),
-        'public_envs': Environment.objects.filter(public=True),
+        'private_envs': Environment.objects.for_user_id(request.user.pk),
+        'favorite_envs': request.user.favorite_env.all(),
+        'public_envs': Environment.objects.filter(public=True), #FIXME this can be large, very large
     }
-    return render_to_response('environment/index.html', ctx,
-        context_instance=RequestContext(request))
+    return TemplateResponse(request, 'environment/index.html', ctx)
+
 
 @login_required
 def add_environment(request):
@@ -58,105 +58,136 @@ def add_environment(request):
     ctx = {
         'form': form
     }
-    return render_to_response('environment/add_environment.html', ctx,
-        context_instance=RequestContext(request))
+    return TemplateResponse(request, 'environment/add_environment.html', ctx)
+
 
 @login_required
 def remove_environment(request, environment_id):
     """Delete an environment and it's relations to images
     """
-    h = Environment.objects.get(id=environment_id)
+    try:
+        h = Environment.objects.for_user_id(request.user.id).get(id=environment_id)
+    except Environment.DoesNotExist:
+        raise http.Http404
     h.delete()
     return redirect('environment.views.index')
+
 
 @login_required
 def favorite_environment(request, environment_id):
     """Make an environment the favorite of this user
     """
-    h = Environment.objects.get(id=environment_id)
-    current_user = request.user
-    u = UserProfile.objects.get(id=current_user.id)
-    u.favorite_env.add(h)
+    try:
+        h = Environment.objects.avail_for_user_id(request.user.id).get(id=environment_id)
+    except Environment.DoesNotExist:
+        raise http.Http404
+    
+    request.user.favorite_env.add(h)
     # make the environment a favorite
     return redirect('environment.views.index')
+
 
 @login_required
 def unfavorite_environment(request, environment_id):
     """Remove an environment from favorites
     """
-    h = Environment.objects.get(id=environment_id)
-    current_user = request.user
-    u = UserProfile.objects.get(id=current_user.id)
-    u.favorite_env.remove(h)
+    try:
+        h = Environment.objects.get(id=environment_id)
+    except Environment.DoesNotExist:
+        raise http.Http404
+    
+    request.user.favorite_env.remove(h)
     # unstar environment
     return redirect('environment.views.index')
+
 
 @login_required
 def clone_environment(request, environment_id):
     """Clone the environment.  Makes a copy of all related images also
     """
-    h = Environment.objects.get(id=environment_id)
+    try:
+        h = Environment.objects.avail_for_user_id(request.user.id).get(id=environment_id)
+    except Environment.DoesNotExist:
+        raise http.Http404
+    #TODO
     return redirect('environment.views.index')
+
 
 @login_required
 def share_environment(request, environment_id):
     """Share the environment with another user. To do: make sure a user owns it
     """
-    h = Environment.objects.get(id=environment_id)
+    try:
+        h = Environment.objects.for_user_id(request.user.id).get(id=environment_id)
+    except Environment.DoesNotExist:
+        raise http.Http404
+    #TODO
     return redirect('environment.views.index')
+
 
 @login_required
 def public_environment(request, environment_id):
     """Make an environment public.
     """
-    h = Environment.objects.get(id=environment_id)
+    try:
+        h = Environment.objects.for_user_id(request.user.id).get(id=environment_id)
+    except Environment.DoesNotExist:
+        raise http.Http404
     h.public = True
     h.save()
     return redirect('environment.views.index')
+
 
 @login_required
 def private_environment(request, environment_id):
     """Make an environment private.
     """
-    h = Environment.objects.get(id=environment_id)
+    try:
+        h = Environment.objects.for_user_id(request.user.id).get(id=environment_id)
+    except Environment.DoesNotExist:
+        raise http.Http404
     h.public = False
     h.save()
     return redirect('environment.views.index')
+
 
 @login_required
 def modify_environment(request, environment_id):
     """Not currently used
     """
-    h = Environment.objects.get(id=environment_id)
+    try:
+        h = Environment.objects.for_user_id(request.user.id).get(id=environment_id)
+    except Environment.DoesNotExist:
+        raise http.Http404
+    #TODO
     return redirect('environment.views.index')
+
 
 @login_required
 def edit_environment(request, environment_id):
     """Edit the name and description of the enviroment.  Associate images to an environment
     """
-    h = Environment.objects.get(id=environment_id)
-    current_user = request.user
-    u = UserProfile.objects.get(id=current_user.id)
+    try:
+        h = Environment.objects.for_user_id(request.user.id).get(id=environment_id)
+    except Environment.DoesNotExist:
+        raise http.Http404
+    
     if request.method == 'POST':
-        form = EnvironmentForm(request.POST)
+        form = EnvironmentForm(request.POST, instance=h)
         if form.is_valid():
             ptype = form.save(commit=False)
-            ptype.owner = request.user
             ptype.save()
             return redirect(reverse('environment.views.index'))
-    initial = {
-        'name': h.name,
-        'description': h.description,
-    }
+    
     ctx = {
         'environment': h,
-        'form': EditEnvironmentForm(initial=initial),
+        'form': EditEnvironmentForm(instance=h),
         'included_images': h.included_images.all(),
-        'private_images': Image.objects.filter(owner__pk=current_user.pk),
-        'favorite_images': u.favorite_image.all(),
+        'private_images': Image.objects.for_user_id(request.user.pk),
+        'favorite_images': request.user.favorite_image.all(),
     }
-    return render_to_response('environment/environment_details.html', ctx,
-        context_instance=RequestContext(request))
+    return TemplateResponse(request, 'environment/environment_details.html', ctx)
+
 
 @login_required
 #@owner_required # TODO
@@ -169,6 +200,7 @@ def attach_image(request, environment_id=None, image_id=None):
     env.save()
     return redirect(reverse('environment.views.edit_environment',
         kwargs={'environment_id': environment_id}))
+
 
 @login_required
 #@owner_required # TODO
--- image/views.py
+++ image/views.py
@@ -15,30 +15,29 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with Science VM. If not, see <http://www.gnu.org/licenses/>.
 
-from django.shortcuts import render_to_response, redirect
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.views.decorators.http import require_http_methods
 from django.core.urlresolvers import reverse
-from django.template import RequestContext
+from django.template.response import TemplateResponse
+from django import http
 from django.contrib import messages
 from django.utils.translation import ugettext as _
 from image.models import Image
-from accounts.models import UserProfile
 from image.forms import ImageForm, EditImageForm
 from scivm import tasks
 
+
 @login_required
 def index(request):
-    current_user = request.user
-    u = UserProfile.objects.get(id=current_user.id)
     ctx = {
-        'private_images': Image.objects.filter(owner__pk=current_user.pk),
-        'favorite_images': u.favorite_image.all(),
-        'public_images': Image.objects.filter(public=True),
+        'private_images': Image.objects.filter(owner__pk=request.user.pk),
+        'favorite_images': request.user.favorite_image.all(),
+        'public_images': Image.objects.filter(public=True), #FIXME this can be very large
     }
-    return render_to_response('image/index.html', ctx,
-        context_instance=RequestContext(request))
+    return TemplateResponse(request, 'image/index.html', ctx)
+
 
 @login_required
 def add_image(request):
@@ -53,57 +52,80 @@ def add_image(request):
     ctx = {
         'form': form
     }
-    return render_to_response('image/add_image.html', ctx,
-        context_instance=RequestContext(request))
+    return TemplateResponse(request, 'image/add_image.html', ctx)
+
 
 @login_required
 def remove_image(request, image_id):
-    h = Image.objects.get(id=image_id)
+    try:
+        h = Image.objects.for_user_id(request.user.id).get(id=image_id)
+    except Image.DoesNotExist:
+        raise http.Http404
+
     h.delete()
     messages.add_message(request, messages.INFO, _('Removed') + ' {0}'.format(
         h.name))
     return redirect('image.views.index')
 
+
 @login_required
 def favorite_image(request, image_id):
     """Make an image the favorite of this user
     """
-    h = Image.objects.get(id=image_id)
-    current_user = request.user
-    u = UserProfile.objects.get(id=current_user.id)
-    u.favorite_image.add(h)
+    try:
+        h = Image.objects.avail_for_user_id(request.user.id).get(id=image_id)
+    except Image.DoesNotExist:
+        raise http.Http404
+    
+    request.user.favorite_image.add(h)
     # make the image a favorite
     return redirect('image.views.index')
+
 
 @login_required
 def unfavorite_image(request, image_id):
     """Remove an image from favorites
     """
-    h = Image.objects.get(id=image_id)
-    current_user = request.user
-    u = UserProfile.objects.get(id=current_user.id)
-    u.favorite_image.remove(h)
+    try:
+        h = Image.objects.get(id=image_id)
+    except Image.DoesNotExist:
+        raise http.Http404
+    
+    request.user.favorite_image.remove(h)
     # unstar image
     return redirect('image.views.index')
 
+
 @login_required
 def clone_image(request, image_id):
-    h = Image.objects.get(id=image_id)
-    # clone the image
+    try:
+        h = Image.objects.avail_for_user_id(request.user.id).get(id=image_id)
+    except Image.DoesNotExist:
+        raise http.Http404
+    
+    #TODO clone the image
     return redirect('image.views.index')
 
 @login_required
 def share_image(request, image_id):
     """Share the image with another user. To do: make sure a user owns it
     """
-    h = Image.objects.get(id=image_id)
+    try:
+        h = Image.objects.for_user_id(request.user.id).get(id=image_id)
+    except Image.DoesNotExist:
+        raise http.Http404
+    #TODO 
     return redirect('image.views.index')
 
 @login_required
 def public_image(request, image_id):
     """Make an image public. To do: make sure a user owns it
     """
-    h = Image.objects.get(id=image_id)
+    try:
+        h = Image.objects.for_user_id(request.user.id).get(id=image_id)
+    except Image.DoesNotExist:
+        raise http.Http404
+    
     h.public = True
     h.save()
     return redirect('image.views.index')
@@ -112,14 +134,22 @@ def public_image(request, image_id):
 def private_image(request, image_id):
     """Make an image private.  To do: make sure user owns it
     """
-    h = Image.objects.get(id=image_id)
+    try:
+        h = Image.objects.for_user_id(request.user.id).get(id=image_id)
+    except Image.DoesNotExist:
+        raise http.Http404
+    
     h.public = False
     h.save()
     return redirect('image.views.index')
 
 @login_required
 def modify_image(request, image_id):
-    h = Image.objects.get(id=image_id)
+    #FIXME permissions,  what can be cloned?
+    try:
+        h = Image.objects.for_user_id(request.user.id).get(id=image_id)
+    except Image.DoesNotExist:
+        raise http.Http404
     # clone the image
     return redirect('image.views.index')
 
@@ -127,29 +157,26 @@ def modify_image(request, image_id):
 def edit_image(request, image_id):
     """Edit the name and description of the enviroment.  Associate images to an image
     """
-    h = Image.objects.get(id=image_id)
-    current_user = request.user
-    u = UserProfile.objects.get(id=current_user.id)
+    try:
+        h = Image.objects.for_user_id(request.user.id).get(id=image_id)
+    except Image.DoesNotExist:
+        raise http.Http404
+    
     if request.method == 'POST':
-        form = ImageForm(request.POST)
+        form = ImageForm(request.POST, instance=h)
         if form.is_valid():
             ptype = form.save(commit=False)
-            ptype.owner = request.user
             ptype.save()
             return redirect(reverse('image.views.index'))
-    initial = {
-        'name': h.name,
-        'description': h.description,
-    }
     ctx = {
         'image': h,
-        'form': EditImageForm(initial=initial),
+        'form': EditImageForm(instance=h),
     }
-    return render_to_response('image/image_details.html', ctx,
-        context_instance=RequestContext(request))
+    return TemplateResponse(request, 'image/image_details.html', ctx)
 
 @login_required
 def import_image(request):
+    #FIXME permission check?
     repo = request.POST.get('repo_name')
     if repo:
         tasks.import_image.delay(repo)
@@ -159,6 +186,7 @@ def import_image(request):
 
 @login_required
 def build_image(request):
+    #FIXME permission check?
     path = request.POST.get('path')
     tag = request.POST.get('tag', None)
     if path:
